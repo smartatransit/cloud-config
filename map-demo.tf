926b9b56b6f80056148867355d62186258551e0a
Xander Flood <xander.t.flood@gmail.com>
2020-01-02 13:09:19 -0500

    add API_KEY env to map-demo

--- map-demo.tf
+++ map-demo.tf
@@ -1,3 +1,7 @@
+variable "map_demo_api_key" {
+  type = "string"
+}
+
 module "map-demo" {
   source = "./modules/service"
 
@@ -5,6 +9,10 @@ module "map-demo" {
   subdomain = "map-demo"
   image     = "smartatransit/map-demo:latest"
   port      = 4000
+
+  env = {
+    API_KEY = var.map_demo_api_key
+  }
 
   traefik_network_name = docker_network.traefik.id
 }
