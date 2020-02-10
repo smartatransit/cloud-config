9bdcda3d6d0bf82d48ebb97715c5fcef09ba2085
Bipol Alam <bipolalam@gmail.com>
2020-02-10 17:56:43 -0500

    add env var

--- smartaslack.tf
+++ smartaslack.tf
@@ -2,6 +2,10 @@ variable "smartaslack_marta_api_key" {
   type = "string"
 }
 
+variable "smartaslack_signing_key" {
+  type = "string"
+}
+
 module "smartaslack" {
   source = "./modules/service"
 
@@ -11,7 +15,8 @@ module "smartaslack" {
   port      = 3000
 
   env = {
-    MARTA_API_KEY = var.smartaslack_marta_api_key
+    MARTA_API_KEY     = var.smartaslack_marta_api_key
+    SLACK_SIGNING_KEY = var.smartaslack_signing_key
   }
 
   traefik_network_name = docker_network.traefik.id
