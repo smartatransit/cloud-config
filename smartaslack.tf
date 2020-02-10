a23f8e58c855b975f311c4769205b2ad11271c92
Bipol Alam <bipolalam@gmail.com>
2020-02-10 18:07:22 -0500

    bump smartaslack (#38)
    * bump
    
    * add env var

--- smartaslack.tf
+++ smartaslack.tf
@@ -2,16 +2,21 @@ variable "smartaslack_marta_api_key" {
   type = "string"
 }
 
+variable "smartaslack_signing_key" {
+  type = "string"
+}
+
 module "smartaslack" {
   source = "./modules/service"
 
   name      = "smartaslack"
   subdomain = "smartaslack"
-  image     = "smartatransit/smartaslack:build-15"
+  image     = "smartatransit/smartaslack:build-17"
   port      = 3000
 
   env = {
-    MARTA_API_KEY = var.smartaslack_marta_api_key
+    MARTA_API_KEY     = var.smartaslack_marta_api_key
+    SLACK_SIGNING_KEY = var.smartaslack_signing_key
   }
 
   traefik_network_name = docker_network.traefik.id
