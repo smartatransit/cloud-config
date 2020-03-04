6ac72c850bb48aca7ecf343256a3a4d4bae00d2d
Xander Flood <xander.t.flood@gmail.com>
2020-03-04 18:35:08 -0500

    transit-stream!

---
+++ transit-stream.tf
@@ -0,0 +1,34 @@
+//// POSTGRES ////
+resource "random_password" "transit_stream_postgres" {
+  length = 64
+}
+
+resource "postgresql_role" "transit_stream" {
+  name     = "transit_stream"
+  login    = true
+  password = random_password.transit_stream_postgres
+}
+
+resource "postgresql_grant" "transit_stream" {
+  database    = postgresql_database.scrapedumper.name
+  role        = postgresql_role.transit_stream.name
+  schema      = "public"
+  object_type = "table"
+  privileges  = ["SELECT"]
+}
+
+//// SERVICE ////
+module "transit-stream" {
+  source = "./modules/service"
+
+  name      = "transit-stream"
+  subdomain = "transit-stream"
+  image     = "smartatransit/transit-stream:latest"
+  port      = 4000
+
+  env = {
+    PGPASSWORD_FILE = random_password.transit_stream_postgres.result
+  }
+
+  traefik_network_name = docker_network.traefik.id
+}
