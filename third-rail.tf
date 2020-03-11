84dea9437dc96862cd14d332af85ae59e9cb183b
Bipol Alam <bipolalam@gmail.com>
2020-03-11 18:30:44 -0400

    add tf file

---
+++ third-rail.tf
@@ -0,0 +1,29 @@
+variable "marta_api_key" {
+  type = "string"
+}
+
+variable "third_rail_twitter_client_id" {
+  type = "string"
+}
+
+variable "third_rail_twitter_client_secret" {
+  type = "string"
+}
+
+//// SERVICE ////
+module "third-rail" {
+  source = "./modules/service"
+
+  name      = "third-rail"
+  subdomain = "third-rail"
+  image     = "smartatransit/third-rail:build-15"
+  port      = 5000
+
+  env = {
+    MARTA_API_KEY             = var.marta_api_key
+    TWITTER_CLIENT_ID         = var.third_rail_twitter_client_id
+    TWITTER_CLIENT_SECERT     = var.third_rail_twitter_client_secret
+  }
+
+  traefik_network_name = docker_network.traefik.id
+}
