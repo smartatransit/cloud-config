0865d40be51bed717b056fbdfc056d5fcfdc1525
Xander Flood <xander.t.flood@gmail.com>
2020-05-01 13:42:59 -0400

    fix typo

--- variables.tf
+++ variables.tf
@@ -8,5 +8,5 @@ locals {
   services_domain = "services.${var.smarta_domain}"
   production_host = "host1.${var.smarta_domain}"
   postgres_host   = local.production_host
-  docker_host     = lcoal.production_host
+  docker_host     = local.production_host
 }
